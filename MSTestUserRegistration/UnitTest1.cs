05c4014d66062be4aff382906662ce14eadab635
Vishal Singh <VishalSingh1597>
2021-05-03 22:33:56 +0530

    Added Use lambda function to validate user entry

--- MSTestUserRegistration/UnitTest1.cs
+++ MSTestUserRegistration/UnitTest1.cs
@@ -7,106 +7,86 @@ namespace MSTestUserRegistration
     [TestClass]
     public class Test
     {
+        private readonly UserDetail User;
+        public Test()//parameterless constructor 
+        {
+            User = new UserDetail(); //create object
+        }
         /// <summary>
         /// Given Valid First Name should return true
         /// </summary>
         [TestMethod]
-        public void GivenFirstNameShouldReturnHappy()
+        public void validateFirstName()//method
         {
-            try
-            {
-                //Act
-                bool output = UserDetail.validateFirstName("Vishal");
-            }
+            var Result = User.validateFirstName("Vishal");
+            Assert.AreEqual(true, Result);
+        }
 
-            catch (UserRegistrationException e)
-            {
-                //Assert
-                Assert.AreEqual("Invalid First Name,Name should be first letter capital", e.Message);
-            }
+        [TestMethod]
+        public void validateLastName()
+        {
+            var Result = User.validateLastName("Singh");
+            Assert.AreEqual(true, Result);
+        }
+
+        [TestMethod]
+        public void Email_Test()
+        {
+            var Result = User.validateEmail("vishal1997@gmail.com");
+            Assert.AreEqual(true, Result);
+        }
+
+        [TestMethod]
+        public void Mobile_Test()
+        {
+            var Result = User.validateMobileNo("91 9930315160");
+            Assert.AreEqual(true, Result);
+        }
+
+        [TestMethod]
+        public void Password_Test()
+        {
+            var Result = User.validatePassword("Vishu@15");
+            Assert.AreEqual(true, Result);
+
         }
 
         /// <summary>
-        /// Given Valid Last Name should return true
-        /// </summary>
-
-        /// <summary>
-        /// Given Valid Last Name should return true
+        /// First the name using lambda expression.
         /// </summary>
         [TestMethod]
-        public void GivenLastNameShouldReturnHappy()
+        public void FirstNameUsingLambdaExpression()
         {
-            try
-            {
-                bool output = UserDetail.validateLastName("Singh");
-            }
-            catch (UserRegistrationException e)
-            {
-                Assert.AreEqual("Invalid Last Name,Name should be first letter capital", e.Message);
-            }
+            var Result = User.validateFirstName("Vishu");
+            Assert.AreEqual(true, Result);
         }
 
-        /// <summary>
-        /// Given Valid Email should return true
-        /// </summary>
         [TestMethod]
-        public void GivenValidEmailIdShouldReturnHappy()
+        public void LastNameUsingLambdaExpression()
         {
-            //Arrange
-            bool expected = true;
-            //Act
-            bool output = UserDetail.validateEmail("abc@gmail.com");
-            // Assert
-            Assert.AreEqual(expected, output);
+            var Result = User.validateLastName("Singh");
+            Assert.AreEqual(true, Result);
         }
 
-
-        /// <summary>
-        /// Given Valid Mobile No should return true
-        /// </summary>
         [TestMethod]
-        public void GivenValidMobileShouldReturnHappy()
+        public void EmailUsingLambdaExpression()
         {
-            bool expected = true;
-            bool output = UserDetail.validateMobileNo("91 9930315160");
-            Assert.AreEqual(expected, output);
+            var Result = User.validateEmail("Singh@777gmail.com");
+            Assert.AreEqual(true, Result);
         }
 
-        /// <summary>
-        /// Given Valid Password should return true
-        /// </summary>
         [TestMethod]
-        public void GivenValidPasswordShouldReturnHappy()
+        public void MobileNumberUsingLabdaExpression()
         {
-            bool expected = true;
-            bool output = UserDetail.validatePassword("Vishu@123");
-            Assert.AreEqual(expected, output);
+            var Result = User.validateMobileNo("91 9967899565");
+            Assert.AreEqual(true, Result);
         }
 
-        /// <summary>
-        /// Test case for all valid and invalid emails
-        /// </summary>
-        /// <param name="email></param>
-        [DataTestMethod]
-        [DataRow("abc@yahoo.com")]
-        [DataRow("abc-100@yahoo.com")]
-        [DataRow("abc.100@yahoo.com")]
-        [DataRow("abc111@abc.com")]
-        [DataRow("abc-100@abc.net")]
-        [DataRow("abc.100@abc.com.au")]
-        [DataRow("abc@1.com")]
-        [DataRow("abc@gmail.com.com")]
-        [DataRow("abc+100@gmail.com")]
-        public void GivenValidEmailListShouldReturnsTrue(string email)
+        [TestMethod]
+        public void PasswordUingLambdaExpression()
         {
-            try
-            {
-                var result = UserDetail.validateEmailList(email);
-            }
-            catch (UserRegistrationException e)
-            {
-                Assert.AreEqual("Invalid Email.Email should be in format", e.Message);
-            }
+            var Result = User.validatePassword("Sharma@123");
+            Assert.AreEqual(true, Result);
         }
     }
 }
